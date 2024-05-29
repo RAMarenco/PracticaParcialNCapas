93f950503d6d772d513185661503376430deea2d
RAMarenco <00080121@uca.edu.sv>
2024-05-28 18:47:27 -0600

    initial commit

---
+++ OnlineCourseSystem/src/test/java/org/happybaras/onlinecoursesystem/OnlineCourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.happybaras.onlinecoursesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
